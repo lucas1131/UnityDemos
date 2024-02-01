db49de603be9ad121a7e3a06b6e295b3e3bf6557
Lucas Soares <lucas.alexandre-soares@kolibrigames.com>
2024-01-31 23:09:51 -0300

    2D simplex noise

--- Assets/Noise/Scripts/Simplex/Noise.Simplex.cs
+++ Assets/Noise/Scripts/Simplex/Noise.Simplex.cs
@@ -8,8 +8,8 @@ public static partial class Noise {
 
 	public struct Simplex1D<G> : INoise where G : struct, IGradient {
 
-		static float4 Kernel(SmallXXHash4 hash, float4 latticeX0, float4x3 positions){
-			float4 x = positions.c0 - latticeX0;
+		static float4 Kernel(SmallXXHash4 hash, float4 latticeX, float4x3 positions){
+			float4 x = positions.c0 - latticeX;
 			float4 falloff = 1f - x*x;
 			falloff = falloff*falloff*falloff; // Cube this function so its C2-continuous
 			return falloff * default(G).Evaluate(hash, x);
@@ -30,8 +30,42 @@ public static partial class Noise {
 
 	public struct Simplex2D<G> : INoise where G : struct, IGradient {
 
+		static float4 Kernel(SmallXXHash4 hash, float4 latticeX, float4 latticeZ, float4x3 positions){
+			float square2TriangleSkew = 0.211324f; // (3f - sqrt(3f))/6f;
+
+			float4 unskew = (latticeX + latticeZ) * square2TriangleSkew;
+			float4 x = positions.c0 - latticeX + unskew;
+			float4 z = positions.c2 - latticeZ + unskew;
+
+
+			float4 falloff = 0.5f - x*x - z*z;
+			falloff = falloff*falloff*falloff*8f; // Cube this function so its C2-continuous
+			return max(0f, falloff) * default(G).Evaluate(hash, x, z);
+		}
+
 		public float4 GetNoise4(float4x3 positions, SmallXXHash4 hash, int frequency) {
-			return default(G).EvaluateCombined(0f);
+			var gradient = default(G);
+
+			positions *= frequency*(1f/sqrt(3f));
+			float triangle2SquareSkew = 0.366025f; // (sqrt(3f) - 1f)/2f;
+			float4 skew = (positions.c0 + positions.c2) * triangle2SquareSkew;
+			float4 skewX = positions.c0 + skew;
+			float4 skewZ = positions.c2 + skew;
+
+			int4 latticeX0 = (int4) floor(skewX);
+			int4 latticeZ0 = (int4) floor(skewZ);
+			int4 latticeX1 = latticeX0 + 1;
+			int4 latticeZ1 = latticeZ0 + 1;
+
+			SmallXXHash4 hx0 = hash.Eat(latticeX0);
+			SmallXXHash4 hx1 = hash.Eat(latticeX1);
+
+			return gradient.EvaluateCombined(
+				Kernel(hx0.Eat(latticeZ0), latticeX0, latticeZ0, positions) +
+				Kernel(hx0.Eat(latticeZ1), latticeX0, latticeZ1, positions) +
+				Kernel(hx1.Eat(latticeZ0), latticeX1, latticeZ0, positions) +
+				Kernel(hx1.Eat(latticeZ1), latticeX1, latticeZ1, positions)
+			);
 		}
 	}
 
